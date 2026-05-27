4bf7e4f9761982ca6ccaa5469544b32ada6db765
panyfmc <paulavanessa010@gmail.com>
2026-05-27 17:11:31 -0300

    :sparkles: feat: servico para busca de um produto pelo nome

--- Services/ProdutoService.cs
+++ Services/ProdutoService.cs
@@ -5,25 +5,63 @@ namespace EcommerceApi.Services;
 
 public class ProdutoService
 {
-    private readonly AppDbContext _context;
-    public ProdutoService(AppDbContext context)
+    private readonly AppDbContext _produto;
+    public ProdutoService(AppDbContext produto)
     {
-        _context = context;
+        _produto = produto;
     }
-    public async Task<Produto?> BuscarPorId(Guid id)
+
+    // funcao para filtrar o produto pelo nome
+    public async Task<Produto?> BuscarPorNome(string nome)
     {
-        return await _context.Produtos .FirstOrDefaultAsync(p => p.Id == id);
+        return await _produto.Produtos .FirstOrDefaultAsync(p => p.Nome == nome);
     }
+
+    // funcao para listar todos os produtos
     public async Task<List<Produto>> ListarAsync()
     {
-        return await _context.Produtos.ToListAsync();
+        return await _produto.Produtos.ToListAsync();
     }
+    
+    // funcao para criar um novo produto
     public async Task<Produto> CriarAsync(Produto produto)
     {
         if(produto.Valor <= 0) throw new Exception("O valor precisa ser maior que zero");
         produto.Id = Guid.NewGuid();
-        _context.Produtos.Add(produto);
-        await _context.SaveChangesAsync();
+        _produto.Produtos.Add(produto);
+        await _produto.SaveChangesAsync();
         return produto;
     }
+
+    // funcao para editar o valor de um produto pelo nome
+    public async Task<ProdutoResponseDto?> EditarProdutoAsync(string nome, decimal valor)
+    {
+        var produto = await _produto.Produtos.FirstOrDefaultAsync(p => p.Nome == nome);
+        if (produto is null) return null;
+
+        if (valor <= 0) throw new Exception("O valor precisa ser maior que zero");
+
+        produto.Valor = valor;
+        await _produto.SaveChangesAsync();
+
+        return new ProdutoResponseDto
+        {
+            Id = produto.Id,
+            Nome = produto.Nome,
+            Valor = produto.Valor
+        };
+    }
+
+    // funcao para deletar um produto pelo nome
+    public async Task<bool> DeletarAsync(string nome) 
+    {
+        var produto = await _produto.Produtos
+            .FirstOrDefaultAsync(p => p.Nome == nome);
+        if (produto is null) return false;
+        _produto.Produtos.Remove(produto);
+        await _produto.SaveChangesAsync();
+
+        return true;
+    }
+    
 }
