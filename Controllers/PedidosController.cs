478c05064bad08b079ee5d1b064e9c9836cc3734
panyfmc <paulavanessa010@gmail.com>
2026-05-27 01:43:31 -0300

    :sparkles: feat: cria endpoints dos pedidos

---
+++ Controllers/PedidosController.cs
@@ -0,0 +1,50 @@
+using EcommerceApi.DTOs;
+using EcommerceApi.Services;
+using Microsoft.AspNetCore.Mvc;
+namespace EcommerceApi.Controllers;
+
+[ApiController] // definido como 'pedidos'
+[Route("api/[controller]")]  // api/pedidos
+public class PedidosController : ControllerBase
+{
+    private readonly PedidoService _pedidoService;
+    public PedidosController(PedidoService pedidoService)
+    {
+        _pedidoService = pedidoService;
+    }
+
+    [HttpGet]   
+    // faz retornar a resposta do servidor
+    public async Task<IActionResult> Listar()
+    {
+        var pedidos = await _pedidoService.ListarAsync();
+        return Ok(pedidos); // retorna status HTTP OK
+    }
+
+    [HttpGet("{id}")]
+    // guarda o id da rota 
+    public async Task<IActionResult> BuscarPorId(Guid id)
+    {
+        var pedido = await _pedidoService.BuscarPorIdAsync(id);
+        if (pedido == null) return NotFound("Pedido não encontrado.");
+        return Ok(pedido); 
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Criar(CriarPedidoDto dto)
+    {
+        try
+        {
+            var pedido = await _pedidoService.CriarAsync(dto);
+            return CreatedAtAction(     // retorna status http Created
+                nameof(BuscarPorId),
+                new {id = pedido.Id},
+                pedido
+            );
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);  // retorna status HTTP Bad Request
+        }
+    }
+}
