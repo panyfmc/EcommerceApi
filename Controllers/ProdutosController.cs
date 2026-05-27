9db35df67362f41a82a15715890c6336d6946daf
panyfmc <paulavanessa010@gmail.com>
2026-05-27 17:10:00 -0300

    :sparkles: feat: metodo para busca de um produto pelo nome

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -4,15 +4,17 @@ using Microsoft.AspNetCore.Mvc;
 namespace EcommerceApi.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/produtos")]
 public class ProdutosController : ControllerBase
 {
-    private readonly ProdutoService _produtoService;
+    private readonly ProdutoService _produtoService;    
+
     public ProdutosController(ProdutoService produtoService)
     {
         _produtoService = produtoService;
     }
 
+    // lista todos os produtos registrados
     [HttpGet]
     public async Task<IActionResult> Listar()
     {
@@ -21,6 +23,16 @@ public class ProdutosController : ControllerBase
         return Ok(produtos);
     }
 
+    // busca um produto pelo nome
+    [HttpGet("{nome}")]
+    public async Task<IActionResult> BuscarPornome(string nome)
+    {
+        var produto = await _produtoService.BuscarPorNome(nome);
+        if (produto == null) return NotFound("produto não encontrado.");
+        return Ok(produto); 
+    }
+
+    // adiciona um novo produto
     [HttpPost]
     public async Task<IActionResult> Criar(Produto produto)
     {
@@ -38,5 +50,30 @@ public class ProdutosController : ControllerBase
         {
             return BadRequest(ex.Message);
         }
+    }
+
+    // atualizar o valor do produto
+    [HttpPatch("{nome}/valor")] 
+    public async Task<IActionResult> EditarProduto(string nome, [FromBody] decimal valor)
+    {
+        try
+        {
+            var produto = await _produtoService.EditarProdutoAsync(nome, valor);
+            if (produto is null) return NotFound("Produto não encontrado.");
+            return Ok(produto);    
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    // delete do produto
+    [HttpDelete("{nome}")]
+    public async Task<IActionResult> Deletar(string nome)
+    {
+        var deletado = await _produtoService.DeletarAsync(nome);
+        if (!deletado) return NotFound("Produto não encontrado.");
+        return NoContent(); 
     }
 }
