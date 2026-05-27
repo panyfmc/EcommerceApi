d1828e15cd56ae401d680c2e2c005e8b42ea0194
panyfmc <paulavanessa010@gmail.com>
2026-05-27 13:23:24 -0300

    :sparkles: feat: modelo para o produto item

---
+++ Models/PedidoItem.cs
@@ -0,0 +1,11 @@
+namespace EcommerceApi.Models;
+
+public class PedidoItem
+{
+    public Guid Id {get; set;}
+    public Guid ProdutoId {get; set;}
+    public Produto Produto {get; set;} = null!;
+    public int Quantidade {get; set;}
+    public decimal ValorUnico {get; set;}
+    public decimal ValorTotal => Quantidade * ValorUnico; 
+}
