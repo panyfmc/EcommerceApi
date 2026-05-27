9278e90e692a53a13f5d701252b00b37638edbb3
panyfmc <paulavanessa010@gmail.com>
2026-05-27 11:26:57 -0300

    :broom: cleanup: nome em atualizar os status de pedido

--- DTOs/AtualizarPedidoDto.cs
+++ DTOs/AtualizarPedidoDto.cs
@@ -4,6 +4,5 @@ namespace EcommerceApi.DTOs;
 // atualiza os status do pedido
 public class AtualizarPedidoDto
 {
-    public string Comprador {get; set;} = string.Empty;
     public StatusPedido Status {get; set;}
 }
