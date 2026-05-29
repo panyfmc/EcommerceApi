9b940a1d8d0b66d5be3219e8bd25bfe344a48b14
panyfmc <paulavanessa010@gmail.com>
2026-05-28 21:56:27 -0300

    :sparkles: feat: add migrations automatica para o docker

--- Program.cs
+++ Program.cs
@@ -5,12 +5,15 @@ using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// --- SERVIÇOS ---
 builder.Services.AddScoped<PedidoService>();
+builder.Services.AddScoped<ProdutoService>();
 
 builder.Services.AddControllers()
     .ConfigureApiBehaviorOptions(options =>
     {
-        options.SuppressModelStateInvalidFilter = true; // desabilita rejeição automatica em status de pedidos incorretos
+        options.SuppressModelStateInvalidFilter = true; 
     })
     .AddJsonOptions(options =>
     {
@@ -19,14 +22,17 @@ builder.Services.AddControllers()
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ProdutoService>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection"),
         sqlOptions =>
         {
-            sqlOptions.EnableRetryOnFailure();
+    
+            sqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 5,
+                maxRetryDelay: TimeSpan.FromSeconds(5),
+                errorNumbersToAdd: null);
         }
     )
 );
@@ -34,14 +40,40 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddHealthChecks();
 var app = builder.Build();
 
+// --- MIDDLEWARES ---
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ecommerce Api V1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.MapHealthChecks("/health");
+
+// --- SEED / MIGRATION AUTOMÁTICA ---
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>(); 
+        
+        Console.WriteLine("Verificando conexão e aplicando Migrations no Docker...");
+        context.Database.Migrate(); 
+        Console.WriteLine("Banco de dados e Migrations aplicados com sucesso!");
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ocorreu um erro crítico ao aplicar as migrations automaticamente.");
+        Console.WriteLine($"ERRO CRÍTICO NO DOCKER: {ex.Message}"); 
+    }
+}
+
 app.Run();
